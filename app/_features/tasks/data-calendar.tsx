01a6ed683970152ca6bf7ebade83daa996028ada
indunil-jay <indunil.jayz@gmail.com>
2024-11-10 23:02:20 +0530

    customize react-big-calendar header toolbar

--- app/_features/tasks/data-calendar.tsx
+++ app/_features/tasks/data-calendar.tsx
@@ -13,6 +13,8 @@ import {
 import { useState } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { EventCard } from "./event-card";
+import { Button } from "@/app/_components/ui/button";
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 // import "react-big-calendar/lib/addons/dragAndDrop/styles";
 
 interface DataCalendarProps {
@@ -73,6 +75,10 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
           localizer?.format(date, "EEE", culture) ?? "",
       }}
       components={{
+        toolbar: () => (
+          <CustomToolBar date={value} onNavigate={handleNavigate} />
+        ),
+
         eventWrapper: ({ event }) => (
           <EventCard
             id={event.id}
@@ -84,5 +90,37 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
         ),
       }}
     />
+  );
+};
+
+interface CustomToolBarProps {
+  date: any;
+  onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+}
+
+const CustomToolBar = ({ date, onNavigate }: CustomToolBarProps) => {
+  return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-a justify-center lg:justify-start">
+      <Button
+        size="icon"
+        variant={"secondary"}
+        onClick={() => onNavigate("PREV")}
+        className="flex items-center"
+      >
+        <ChevronLeftIcon className="size-4" />
+      </Button>
+      <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+        <CalendarIcon className="size-4 mr-2" />
+        <p className="text-sm">{format(date, "MMMM yyyy")}</p>
+      </div>
+      <Button
+        size="icon"
+        variant={"secondary"}
+        onClick={() => onNavigate("NEXT")}
+        className="flex items-center"
+      >
+        <ChevronRightIcon className="size-4" />
+      </Button>
+    </div>
   );
 };
